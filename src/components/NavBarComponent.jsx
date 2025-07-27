db88c519e73af0f5415cad11e3dd09270d81f577
rizaldhmarsyah <rizalnurdhmarsyah@gmail.com>
2025-07-27 17:51:34 +0700

    Finalizing React Router paths for Vercel deployment

--- src/components/NavBarComponent.jsx
+++ src/components/NavBarComponent.jsx
@@ -2,25 +2,29 @@ import { useEffect, useState } from "react";
 import flag from "../assets/indonesia.png";
 import { useDispatch, useSelector } from "react-redux";
 import { toogleStatusTab } from "../redux/slices/cartSlice";
-import { Link } from "react-router-dom";
+import { Link } from "react-router-dom"; // Pastikan Link sudah diimpor
 
 const NavBarComponent = () => {
   const [totalQuantity, setTotalQuantity] = useState(0);
   const carts = useSelector((store) => store.cart.items);
   const dispatch = useDispatch();
+
   useEffect(() => {
     let total = 0;
     carts.forEach((item) => (total += item.quantity));
     setTotalQuantity(total);
   }, [carts]);
+
   const [isClick, setisClick] = useState(false);
 
   const toogleNavbar = () => {
     setisClick(!isClick);
   };
+
   const handleOpenTabCart = () => {
     dispatch(toogleStatusTab());
   };
+
   const handleOpenTabCart2 = () => {
     dispatch(toogleStatusTab());
   };
@@ -31,40 +35,42 @@ const NavBarComponent = () => {
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
             <div className="flex-shrink-0">
-              <a
-                href="/"
+              {/* Ganti <a> dengan <Link> untuk logo/nama situs */}
+              <Link
+                to="/"
                 className="text-white no-underline text-3xl font-bold"
               >
                 whatever.
-              </a>
+              </Link>
             </div>
           </div>
           <div className="hidden md:block">
             <div className="ml-4 flex items-center space-x-4">
-              <a
-                href="/"
+              {/* Ganti semua <a> dengan <Link> di navigasi desktop */}
+              <Link
+                to="/"
                 className="text-baseColor hover:text-textColor hover:bg-baseColor rounded-lg no-underline p-2 transition-all"
               >
                 HOME
-              </a>
-              <a
-                href="/backpack"
+              </Link>
+              <Link
+                to="/backpack"
                 className="text-baseColor hover:text-textColor hover:bg-baseColor rounded-lg no-underline p-2 transition-all"
               >
                 BACKPACK
-              </a>
-              <a
-                href="/gear"
+              </Link>
+              <Link
+                to="/gear"
                 className="text-baseColor hover:text-textColor hover:bg-baseColor rounded-lg no-underline p-2 transition-all"
               >
                 GEAR
-              </a>
-              <a
-                href="/experience"
+              </Link>
+              <Link
+                to="/experience"
                 className="text-baseColor hover:text-textColor hover:bg-baseColor rounded-lg no-underline p-2 transition-all"
               >
                 EXPERIENCE
-              </a>
+              </Link>
 
               {/* Shopping Cart */}
               <div onClick={handleOpenTabCart} className="cursor-pointer">
@@ -87,7 +93,7 @@ const NavBarComponent = () => {
           </div>
 
           <div className="md:hidden flex items-center gap-2">
-            {/* Shopping Cart */}
+            {/* Shopping Cart (Mobile) */}
             <div className="block sm:hidden" onClick={handleOpenTabCart2}>
               <i className="fa-solid fa-bag-shopping text-2xl text-baseColor hover:text-textColor hover:bg-baseColor rounded-lg no-underline p-2 transition-all">
                 <span className="fixed top-10 translate-y-[-4px] bg-red-500 text-baseColor text-xs w-4 h-4 rounded-full flex justify-center items-center">
@@ -96,14 +102,14 @@ const NavBarComponent = () => {
               </i>
             </div>
 
-            {/* Profile */}
+            {/* Profile (Mobile) */}
             <Link to={"/profile"} className="cursor-pointer">
               <i className="fa-regular fa-user text-2xl text-baseColor hover:text-textColor hover:bg-baseColor rounded-lg no-underline p-2 transition-all"></i>
             </Link>
             {/* Profile */}
 
             <button
-              className="inline-flex items-center justify-center p-2 rounded-md text-yellow-50 hover:text-yellow-50 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-yellow-50"
+              className="inline-flex items-center justify-center p-2 rounded-md text-yellow-50 hover:text-black hover:bg-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
               onClick={toogleNavbar}
             >
               {isClick ? (
@@ -144,30 +150,31 @@ const NavBarComponent = () => {
       {isClick && (
         <div className="md:hidden">
           <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-            <a
-              href="/"
+            {/* Ganti semua <a> dengan <Link> di navigasi mobile */}
+            <Link
+              to="/"
               className="text-yellow-50 block hover:text-black hover:bg-white rounded-lg no-underline p-2"
             >
               HOME
-            </a>
-            <a
-              href="/backpack"
+            </Link>
+            <Link
+              to="/backpack"
               className="text-yellow-50 block hover:text-black hover:bg-white rounded-lg no-underline p-2"
             >
               BACKPACK
-            </a>
-            <a
-              href="/gear"
+            </Link>
+            <Link
+              to="/gear"
               className="text-yellow-50 block hover:text-black hover:bg-white rounded-lg no-underline p-2"
             >
               GEAR
-            </a>
-            <a
-              href="/experience"
+            </Link>
+            <Link
+              to="/experience"
               className="text-yellow-50 block hover:text-black hover:bg-white rounded-lg no-underline p-2"
             >
               EXPERIENCE
-            </a>
+            </Link>
           </div>
         </div>
       )}
